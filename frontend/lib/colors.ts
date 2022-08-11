55951fd3eddad6b51dd4b7d993d02e4d118ac89f
Kabir Khandpur <kabirk@live.com>
2022-08-11 01:44:51 +0100

    Global store via context

---
+++ frontend/lib/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  red: "rgb(232, 41, 60)",
+  blue: "rgb(85, 150, 230)",
+  green: "rgb(0, 180, 160)",
+  yellow: "rgb(253, 214, 0)",
+  purple: "rgb(175, 110, 232)",
+  pink: "rgb(255, 113, 212)",
+  white: "rgb(255, 255, 255)",
+  midBlue: "rgb(48, 90, 128)",
+  orange: "rgb(255, 146, 73)",
+  lightGreen: "rgb(12, 183, 64)",
+  lightBrown: "rgb(210, 176, 164)",
+};
+
+export default colors;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { StoreProvider } from "../lib/store";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default App;
