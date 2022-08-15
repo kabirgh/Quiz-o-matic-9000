fc79c75025abb476f33f65626af82430fa2e1536
Kabir Khandpur <kabirk@live.com>
2022-08-15 23:58:28 +0100

    Game screen keydown, dynamic font size

--- frontend/pages/game.tsx
+++ frontend/pages/game.tsx
@@ -1,35 +1,68 @@
 import type { NextPage } from "next";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import type { Team } from "../lib/store";
 import { useStore } from "../lib/store";
+import useClientRect from "../lib/useClientRect";
 
 // import styles from './game.module.css'
 
 const Game: NextPage = () => {
-  const { store, setStore } = useStore();
+  const router = useRouter();
+  const { teams } = useStore();
+  const [played, setPlayed] = useState([] as Team[]);
+  const [rect, teamRowRef] = useClientRect();
 
-  const rowSize = 100.0 / store.teams.length;
+  useEffect(() => {
+    const keydownHandler = (event: any) => {
+      switch (event.code) {
+        case "KeyR":
+          setPlayed([]);
+          break;
+        case "KeyS":
+          setPlayed(teams);
+          break;
+        case "Backspace":
+          setPlayed([]);
+          router.push("/");
+          break;
+      }
+    };
+
+    addEventListener("keydown", keydownHandler);
+    return () => {
+      removeEventListener("keydown", keydownHandler);
+    };
+  }, [router, teams]);
+
+  const rowSize = 100.0 / teams.length;
   const cardSize = 0.9 * rowSize;
   const spacerSize = 0.1 * rowSize;
 
   return (
     <div
       style={{
+        fontFamily: "Arvo",
         backgroundColor: "#323232",
         height: "100vh",
         width: "100vw",
         display: "grid",
         gridTemplateColumns: "1fr 8fr 1fr",
-        gridTemplateRows: `${spacerSize}fr ${store.teams
+        gridTemplateRows: `${spacerSize}fr ${teams
           .map((_team) => `${cardSize}fr ${spacerSize}fr`)
           .join(" ")}`,
       }}
     >
-      {store.teams.map((team, index) => {
+      {played.map((team, index) => {
         const i = index + 1;
         const id = `row-${i}`;
         return (
           <div
             id={id}
             key={id}
+            ref={teamRowRef as any}
             style={{
               gridArea: `${2 * i} / 2 / ${2 * i + 1} / 3`,
               backgroundColor: `${team.color}`,
@@ -37,6 +70,10 @@ const Game: NextPage = () => {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              fontSize:
+                rect === null || typeof rect !== "object"
+                  ? 0
+                  : 0.25 * rect.height,
             }}
           >
             {team.name}
