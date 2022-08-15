8908b30e0d65f1d558f816888052293e6bc0b888
Kabir Khandpur <kabirk@live.com>
2022-08-15 23:54:34 +0100

    Minor changes to store and colors

--- frontend/lib/colors.ts
+++ frontend/lib/colors.ts
@@ -1,15 +1,12 @@
 const colors = {
-  red: "rgb(232, 41, 60)",
-  blue: "rgb(85, 150, 230)",
-  green: "rgb(0, 180, 160)",
-  yellow: "rgb(253, 214, 0)",
-  purple: "rgb(175, 110, 232)",
-  pink: "rgb(255, 113, 212)",
-  white: "rgb(255, 255, 255)",
-  midBlue: "rgb(48, 90, 128)",
-  orange: "rgb(255, 146, 73)",
-  lightGreen: "rgb(12, 183, 64)",
-  lightBrown: "rgb(210, 176, 164)",
+  red: "#E8293C",
+  blue: "#5596E6",
+  green: "#00B4A0",
+  yellow: "#FDD600",
+  purple: "#AF6EE8",
+  lightBrown: "#D2B0A4",
+  midBlue: "#305A80",
+  orange: "#FF9249",
 };
 
 export default colors;
--- frontend/lib/store.tsx
+++ frontend/lib/store.tsx
@@ -1,42 +1,25 @@
 import { createContext, useContext, useState } from "react";
 import colors from "./colors";
 
-type Team = {
+export type Team = {
   name: string;
-  color: string;
-};
-
-type Store = {
-  teams: Team[];
-  colors: { [key: string]: string };
+  color: string | null;
 };
 
 const AppContext = createContext({
-  store: {} as Store,
-  setStore: (_store: Store) => {},
+  teams: [] as Team[],
+  setTeams: (_store: Team[]) => {},
 });
 
 export const useStore = () => useContext(AppContext);
 
 export const StoreProvider = ({ children }: { children: any }) => {
-  const [store, setStore] = useState({
-    teams: [
-      { name: "9/9", color: colors.red },
-      { name: "12B", color: colors.blue },
-      { name: "Chixie Dix", color: colors.green },
-      { name: "King in the Lounge", color: colors.yellow },
-      { name: "Jumbo", color: colors.purple },
-      // { name: "Monkey bars", color: colors.pink },
-      // { name: "Alpha Kenny", color: colors.white },
-      // { name: "Zoom angels", color: colors.midBlue },
-      // { name: "Clairvoyant", color: colors.orange },
-      // { name: "Alpaca", color: colors.lightGreen },
-    ],
-    colors: colors,
-  } as Store);
+  const [teams, setTeams] = useState([
+    { name: "", color: colors.red },
+  ] as Team[]);
 
   return (
-    <AppContext.Provider value={{ store, setStore }}>
+    <AppContext.Provider value={{ teams, setTeams }}>
       {children}
     </AppContext.Provider>
   );
