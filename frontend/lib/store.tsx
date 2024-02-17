b0c0d4a2a0c6c68f4a70cc15779dd6db3c7eec83
Kabir Khandpur <kabirk@live.com>
2024-02-17 15:08:06 +0000

    Del colors, make store do nothing

--- frontend/lib/store.tsx
+++ frontend/lib/store.tsx
@@ -1,24 +1,11 @@
 import { createContext, useContext, useState } from "react";
-import { main } from "../wailsjs/wailsjs/go/models";
-import colors from "./colors";
 
-export type Team = main.Team;
-
-const AppContext = createContext({
-  teams: [] as Team[],
-  setTeams: (_store: Team[]) => {},
-});
+const AppContext = createContext({});
 
 export const useStore = () => useContext(AppContext);
 
 export const StoreProvider = ({ children }: { children: any }) => {
-  const [teams, setTeams] = useState([
-    { name: "", color: colors.red, buzzer: undefined }, // undefined instead of null to match generated ts model
-  ] as Team[]);
-
-  return (
-    <AppContext.Provider value={{ teams, setTeams }}>
-      {children}
-    </AppContext.Provider>
-  );
+  // Usage: const [x, setX] = useState(0);
+  // Then pass in x and setX in value prop of AppContext.Provider
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
