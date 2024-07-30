b58c6a8d0b0f2223d12b1604ae7210b3e0a97069
Kabir <kabirk@live.com>
2024-07-30 02:01:27 +0100

    Install tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    // Keep default styling
+    preflight: false,
+  },
+};
