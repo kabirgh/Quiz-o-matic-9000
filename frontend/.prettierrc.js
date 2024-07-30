cc7bbe47ece8b4cd02e5083432992410e3c7d8d8
Kabir <kabirk@live.com>
2024-07-30 21:51:16 +0100

    Install prettier and sort imports plugins

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+module.exports = {
+  // Standard prettier options
+  singleQuote: true,
+  semi: true,
+  // Since prettier 3.0, manually specifying plugins is required
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  // This plugin's options
+  importOrder: ['^@core/(.*)$', '', '^@server/(.*)$', '', '^@ui/(.*)$', '', '^[./]'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+};
